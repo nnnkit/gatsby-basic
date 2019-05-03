7160d762063d0817c02a3fd73fdee55591e7ef8a
nnnkit <iankit17@gmail.com>
2019-05-03 20:31:01 +0530

    add mdx support and add a sample page contact

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,16 @@ module.exports = {
   siteMetadata: {
     title: "Gatsby Blog"
   },
-  plugins: ["gatsby-plugin-emotion", "gatsby-plugin-react-helmet"]
+  plugins: [
+    "gatsby-plugin-emotion",
+    "gatsby-plugin-react-helmet",
+    {
+      resolve: "gatsby-mdx",
+      options: {
+        defaultLayouts: {
+          default: require.resolve("./src/components/layout.js")
+        }
+      }
+    }
+  ]
 };
