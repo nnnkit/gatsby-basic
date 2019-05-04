63a4f0e223492b0c5b1a3a83d0a576c617af1156
nnnkit <iankit17@gmail.com>
2019-05-04 21:18:09 +0530

    add image support for mdx

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,12 +5,15 @@ module.exports = {
   plugins: [
     "gatsby-plugin-emotion",
     "gatsby-plugin-react-helmet",
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sharp",
     {
       resolve: "gatsby-mdx",
       options: {
         defaultLayouts: {
           default: require.resolve("./src/components/layout.js")
-        }
+        },
+        gatsbyRemarkPlugins: [{ resolve: "gatsby-remark-images" }]
       }
     },
     {
@@ -18,6 +21,13 @@ module.exports = {
       options: {
         name: "posts", // name of the folder
         path: "posts" // path of url
+      }
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images", // name of the folder
+        path: "images" // path of url
       }
     }
   ]
